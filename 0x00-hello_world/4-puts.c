c67acb5d10135c9d3824dccb2014108f5cdaee4f
victor chibuzor <chibuzorvictor97@gmail.com>
2023-06-15 14:17:26 -0700

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
