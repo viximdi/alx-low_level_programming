dc2be33d9e57231f96253224225d140b87f60197
victor chibuzor <chibuzorvictor97@gmail.com>
2023-06-15 16:35:14 -0700

    modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,12 @@
 #include <unistd.h>
-/* main - entry point */
+/**
+ * main - Entry point of the program
+ *
+ * Return: Exit status of the program
+ */
 int main(void)
 {
-	char *strings = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-		write(2, strings, 65);
+	char *strgs = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+		write(2, strgs, 65);
 	return (1);
 }
