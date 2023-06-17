142f9fe70b40ee05d394e6a0f5a97cb9c446485c
victor chibuzor <chibuzorvictor97@gmail.com>
2023-06-16 18:33:08 -0700

    modified

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,20 +10,16 @@ for (num1 = 0; num1 < 100; num1++)
 {
 for (num2 = num1; num2 < 100; num2++)
 {
-if (num1 / 10 < num2 / 10 ||
-		(num1 / 10 == num2 / 10 && num1 % 10 <= num2 % 10))
-{
 putchar('0' + num1 / 10);
 putchar('0' + num1 % 10);
 putchar(' ');
 putchar('0' + num2 / 10);
 putchar('0' + num2 % 10);
 
-if (num1 != 98 || num2 != 99)
+if (num1 != 99 || num2 != 99)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
@@ -36,6 +32,5 @@ putchar(' ');
 int main(void)
 {
 print_combinations();
-putchar('\n');
 return (0);
 }
