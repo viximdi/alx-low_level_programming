698839f8782aa5e7ca526cfc9f5798a1bfe67d46
victor chibuzor <chibuzorvictor97@gmail.com>
2023-06-16 17:37:02 -0700

    modified

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,21 @@ int digit1, digit2;
 
 for (digit1 = 0; digit1 < 10; digit1++)
 {
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+for (digit2 = 0; digit2 < 10; digit2++)
+{
+if (digit1 != digit2)
+{
+if (digit1 < digit2)
 {
 putchar('0' + digit1);
 putchar('0' + digit2);
+
 if (digit1 != 8 || digit2 != 9)
 {
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 }
