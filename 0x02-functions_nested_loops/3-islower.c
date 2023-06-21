2a4b474124f010dcf83577ab131e559e591c43da
victor chibuzor <chibuzorvictor97@gmail.com>
2023-06-20 17:54:19 -0700

    modified

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,10 +4,12 @@
  *
  * Return: 1 if c is lowercase, 0 otherwise.
  */
-int _islower(int c) {
-    if (c >= 'a' && c <= 'z') {
-        return 1;
-    } else {
-        return 0;
-    }
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+		return (0);
 }
