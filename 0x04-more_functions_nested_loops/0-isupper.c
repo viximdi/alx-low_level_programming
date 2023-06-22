8fa73d7fb07db4eafc610329a227d922add351f2
victor chibuzor <chibuzorvictor97@gmail.com>
2023-06-22 09:28:30 -0700

    modified

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,12 @@
 /**
  * _isupper -  checks for uppercase character
  * @c: character to be checked
- * return: 1 if c is uppercase, Returns 0 otherwise
+ * Return: 1 if c is uppercase, Returns 0 otherwise
  */
 int _isupper(int c)
 {
 
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
