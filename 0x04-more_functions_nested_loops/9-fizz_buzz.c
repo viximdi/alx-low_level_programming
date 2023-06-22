99ce88b56935ecb6a4bb2550973e5bc8f5c9f48a
victor chibuzor <chibuzorvictor97@gmail.com>
2023-06-22 16:46:06 -0700

    modified

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,23 @@ int fizz_buzz_number(int n)
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
-			putchar(' ');
-			continue;
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			putchar(' ');
-			continue;
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			putchar(' ');
-			continue;
 		}
 		else
 		{
 			printf("%d", i);
-			putchar(' ');
+		}
+
+		if (i < 100)
+		{
+			printf(" ");
 		}
 
 	}
