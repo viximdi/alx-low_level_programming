d5fce17fba6a74b82a1c334ed9e7ef0be6e558ee
victor chibuzor <chibuzorvictor97@gmail.com>
2023-06-26 15:51:11 -0700

    modified

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,22 +9,14 @@ void puts_half(char *str)
 	int i = 0, n;
 
 	while (str[i] != '\0')
-	{
 		i++;
-	}
 	if (i + 1 % 2 != '0')
-	{
 		n = (i - 1) / 2;
-	}
 	else
-	{
 		n = (i / 2);
-		n++;
-	}
+	n++;
 
 	for (i = n; str[i] != '\0'; i++)
-	{
 		_putchar(str[i]);
-	}
 	_putchar('\n');
 }
