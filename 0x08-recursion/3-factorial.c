547b862a0fec6558d6a4785e4740c3e98d7e177b
victor chibuzor <chibuzorvictor97@gmail.com>
2023-07-05 09:48:25 -0700

    modified

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,9 +10,13 @@ int factorial(int n)
 	{
 		return (-1);
 	}
+	else if (n == 0)
+	{
+		return (1);
+	}
 	else
 	{
-		fact = n * factorial(n-1);
+		fact = n * factorial(n - 1);
 	}
 	return (fact);
 }
