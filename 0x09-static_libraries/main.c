d6f82e26a1253178643d852724d0a78227e85a15
victor chibuzor <chibuzorvictor97@gmail.com>
2023-07-07 11:01:46 -0700

    modidfied yet again

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function serves as the entry point of the program.
+ *              It prints a quote about goals and hacking by Kevin Mitnick.
+ *              The quote is followed by the attribution to Kevin Mitnick.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n"
+			"\t- Kevin Mitnick");
+	return (0);
+}
