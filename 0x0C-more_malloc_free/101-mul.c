ddafdc4348cae5f76603b20fb2735def30925e43
victor chibuzor <chibuzorvictor97@gmail.com>
2023-07-14 14:09:50 -0700

    modified

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * _print - moves a string one place to the left and prints the string
  * @str: string to move
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * _calloc - allocates memory for an array of @nmemb elements of
  * @size bytes each and returns a pointer to the allocated memory.
