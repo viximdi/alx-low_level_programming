0345b65a4be70d8533748075a01dceb28aa0d0eb
victor chibuzor <chibuzorvictor97@gmail.com>
2023-07-17 17:26:09 -0700

    modified

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,16 +2,12 @@
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  *
- * Description: This function creates a new dog with the provided
- * name, age, and owner information. It dynamically allocates memory
- * for the new dog structure and its associated name and owner strings.
- *
- * Return: Pointer to the newly created dog structure
+ * Return: pointer to new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -20,31 +16,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-
 	for (nl = 0; name[nl]; nl++)
 		;
 	nl++;
-
 	dog->name = malloc(nl * sizeof(char));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < nl; i++)
 		dog->name[i] = name[i];
-
 	dog->age = age;
-
 	for (ol = 0; owner[ol]; ol++)
 		;
 	ol++;
-
 	dog->owner = malloc(ol * sizeof(char));
 	if (dog->owner == NULL)
 	{
@@ -52,9 +41,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < ol; i++)
 		dog->owner[i] = owner[i];
-
 	return (dog);
 }
