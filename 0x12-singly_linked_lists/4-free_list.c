252dd68ac100305e5ea14848fb4e3ea90a55ff25
Chibuzor victor <chibuzorvictor97@gmail.com>
2023-07-28 01:35:40 +0100

    a function that frees a list_t list.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees the memory allocated for a linked list.
+ * @head: A pointer to the first node of the list.
+ * This function frees the memory for each node and its string (if present).
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		free(head->str);
+		free(head);
+	}
+}
