982342e41ff11b68d0f6d5095116100657f37767
victor chibuzor <chibuzorvictor97@gmail.com>
2023-08-07 18:49:40 -0700

    mod

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,14 +8,16 @@
  * @text_content: A NULL-terminated string to write to the file.
  *
  * Return: 1 on success, -1 on failure.
- *         (file cannot be created, file cannot be written, write "fails", etc...)
+ *         (file cannot be created, file cannot be written,
+ *write "fails", etc...)
  *         The created file must have permissions: rw-------.
  *         If the file already exists, do not change the permissions.
  *         If the file already exists, truncate it.
  *         If filename is NULL, return -1.
  *         If text_content is NULL, create an empty file.
  */
-int create_file(const char *filename, char *text_content) {
+int create_file(const char *filename, char *text_content)
+{
 	int fd, i;
 	ssize_t bytes_written;
 
@@ -26,11 +28,13 @@ int create_file(const char *filename, char *text_content) {
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL) {
+	if (text_content != NULL)
+	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			continue;
 		bytes_written = write(fd, text_content, i);
-		if (bytes_written == -1) {
+		if (bytes_written == -1)
+		{
 			close(fd);
 			return (-1);
 		}
